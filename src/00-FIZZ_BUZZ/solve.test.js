14304a7e17c4335781d5251a9413e6fd6e9b4e69
edido <esuarez@integ-solutions.cl>
2022-07-31 19:48:02 -0400

    more test

--- src/00-FIZZ_BUZZ/solve.test.js
+++ src/00-FIZZ_BUZZ/solve.test.js
@@ -9,3 +9,13 @@ test('calc number is divisible by 3', function () {
   let result = solve.fizzBuzz(6);
   expect(result).toBe('Fizz');
 });
+
+test('calc number is divisible by 5', function () {
+  let result = solve.fizzBuzz(10);
+  expect(result).toBe('Buzz');
+});
+
+test('calc number is not divisible by 3 or 5', function () {
+  let result = solve.fizzBuzz(7);
+  expect(result).toBe(7);
+});
