9ba3c858917fba1787fe6a007fc2cdd65f556789
edido <esuarez@integ-solutions.cl>
2022-07-31 14:06:28 -0400

    adding babel

--- src/00-FIZZ_BUZZ/solve.test.js
+++ src/00-FIZZ_BUZZ/solve.test.js
@@ -1,7 +1,6 @@
-
 const solve = require('./solve');
 
-test ('calc number is divisible by 3 and 5', function() {
-    let result = solve.fizzBuzz(15);
-    expect(result).toBe('FizzBuzz');
+test('calc number is divisible by 3 and 5', function () {
+  let result = solve.fizzBuzz(15);
+  expect(result).toBe('FizzBuzz');
 });
