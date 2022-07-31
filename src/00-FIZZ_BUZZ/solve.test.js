9c9720f8da2e532464bc9961ebccbd2b2da9cefc
edido <esuarez@integ-solutions.cl>
2022-07-31 15:56:50 -0400

    adding new test

--- src/00-FIZZ_BUZZ/solve.test.js
+++ src/00-FIZZ_BUZZ/solve.test.js
@@ -4,3 +4,8 @@ test('calc number is divisible by 3 and 5', function () {
   let result = solve.fizzBuzz(15);
   expect(result).toBe('FizzBuzz');
 });
+
+test('calc number is divisible by 3', function () {
+  let result = solve.fizzBuzz(6);
+  expect(result).toBe('Fizz');
+});
